0422d3104c9bd6334eaa527032a41d2dd5d46640
CYNTHIA-IGIHOZO <ciiginta7@gmail.com>
2024-05-09 08:26:29 +0200

    Blue print

--- src/configs/index.js
+++ src/configs/index.js
@@ -16,7 +16,7 @@ app.get(API_URI, (req, res) => {
 mongoose.connect('mongodb://localhost:27017/safetydriveconnect')
   .then(() => {
     console.log("Database connected successfully");
-    const PORT = process.env.PORT || 5001;
+    const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => {
       console.log(`Server listening on port ${PORT}....`);
     });
